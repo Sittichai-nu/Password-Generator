86ea690c64b96272be4e62a503d6bfdd2a58162a
Nu <nu.chai56@gmail.com>
2023-04-01 18:34:46 -0500

    Refactor password generator code

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,75 +1,170 @@
-var charSelect = [];
-var passwordLength = 0;
-var randomChar = 0;
-var lowerConfirm;
-var upperConfirm;
-var symbConfirm;
-var numConfirm;
-var pass = "";
+// Define arrays for each type of character
+const uppercaseChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowercaseChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const numericChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialChars = [
+  "`",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "=",
+  "+",
+  "|",
+  ";",
+  ":",
+  "'",
+  "?",
+  "/",
+  ".",
+  ",",
+  "<",
+  ">",
+];
 
+// Define variables for user input and generated password
+let passwordLength;
+let useUppercaseChars;
+let useLowercaseChars;
+let useNumericChars;
+let useSpecialChars;
+let generatedPassword = "";
 
-function generatePass() {
-    passLength();
-    style();
-};
-const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symb = ["`","`","!", "@", "#", "$", "%","^","&","*","(",")","-","_","=","+","|",";",":","'","?","/",".",",","<",">"];
-    
-    function passLength() {
-        passwordLength = prompt ("How many passwords characters would you like? "+" you can choose between 8 to 128 characters");
-    
-        console.log(typeof passwordLength);
-        numLength = parseInt(passwordLength);
-        passwordLength = numLength;
-    
-        while ((passwordLength <= 7 || passwordLength >= 129) || (isNaN(passwordLength)) || (typeof passwordLength === "string")) {
-            alert("Password must be between 8 and 128 characters.");
-            passwordLength = prompt("How many passwords characters would you like? " + " You can choose between 8 to 128 characters");
-    
-            console.log(typeof passwordLength);
-            numLength = parseInt(passwordLength);
-            passwordLength = numLength;
-        };
-        console.log(typeof passwordLength);
-        console.log(passwordLength);
-    };
-    
-    function style() {
-        const upperConfirm = confirm('Would you like uppercase letters?');
-        console.log(upperConfirm);
-        const lowerConfirm = confirm('Would you like lowercase letters?');
-        console.log(lowerConfirm);
-        const symbConfirm = confirm('Would you like special charaters?');
-        console.log(symbConfirm);
-        const numConfirm = confirm('Would you like numbers?');
-        console.log(numConfirm);
-    
-        if (confirm) {
-            characters = charSelect.concat(lowerLetters, upperLetters, num, symb);
-            charSelect = characters;
-            console.log(charSelect);
-        };
-    
-        if ((symbConfirm === false) && (numConfirm === false) && (lowerConfirm === false) && (upperConfirm === false)) {
-            alert("You need to select at least one character type to continue.");
-        };
-    
-        for (let i = 0; i < passwordLength; i++) {
-            randomChar = Math.floor(Math.random() * charSelect.length);
-            pass += charSelect[randomChar];
-        };
-    
-        console.log(pass);
-        document.getElementById("password").value = pass;
-    
-    };
-    
-    function copy() {
-        var copyText = document.getElementById("password");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999)
-        document.execCommand("copy");
-        alert("Password Copied!")
-    };
+// Prompt user for password criteria
+function getPasswordCriteria() {
+  passwordLength = parseInt(
+    prompt(
+      "How many characters would you like in your password? (Choose a number between 8 and 128)"
+    )
+  );
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid number between 8 and 128.");
+    getPasswordCriteria();
+    return;
+  }
+
+  useUppercaseChars = confirm("Would you like to include uppercase letters?");
+  useLowercaseChars = confirm("Would you like to include lowercase letters?");
+  useNumericChars = confirm("Would you like to include numeric characters?");
+  useSpecialChars = confirm("Would you like to include special characters?");
+
+  if (
+    !useUppercaseChars &&
+    !useLowercaseChars &&
+    !useNumericChars &&
+    !useSpecialChars
+  ) {
+    alert("Please select at least one type of character.");
+    getPasswordCriteria();
+    return;
+  }
+}
+
+// Generate password based on user input
+function generatePassword() {
+  getPasswordCriteria();
+
+  let availableChars = [];
+  if (useUppercaseChars) {
+    availableChars = availableChars.concat(uppercaseChars);
+  }
+  if (useLowercaseChars) {
+    availableChars = availableChars.concat(lowercaseChars);
+  }
+  if (useNumericChars) {
+    availableChars = availableChars.concat(numericChars);
+  }
+  if (useSpecialChars) {
+    availableChars = availableChars.concat(specialChars);
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+    generatedPassword += availableChars[randomIndex];
+  }
+}
+
+// Display generated password on the page
+function displayPassword() {
+  const passwordElement = document.getElementById("password");
+  passwordElement.value = generatedPassword;
+}
+
+// Copy generated password to clipboard
+function copyPassword() {
+  const passwordElement = document.getElementById("password");
+  passwordElement.select();
+  passwordElement.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Password copied to clipboard!");
+  passwordElement.value = "";
+
+}
+
+function generate() {
+  generatePassword();
+  displayPassword();
+}
